73afa3c92de3b9d78608d8a38472292e62c4b2be
JulienAN <60385286+JulienAN@users.noreply.github.com>
2020-11-16 18:10:33 +0100

    Add files via upload

--- src/JeuxPacman/displayPacman.java
+++ src/JeuxPacman/displayPacman.java
@@ -4,43 +4,66 @@ import GraphicEngine.GraphicEngine;
 
 public class displayPacman {
 
-    private final String curDir = System.getProperty("user.dir");
     private final String scene;
     private final GraphicEngine windows;
+    private final String[] imageLabels = {"PacMan_left","PacMan_right","PacMan_up","PacMan_down","PacMan_death"};
+
 
     public displayPacman(GraphicEngine windows, String nameScene) throws Exception {
         this.windows = windows;
-        this.scene = nameScene;
+        this.scene   = nameScene;
 
-        windows.addImage(this.scene, "pacManLeft", curDir + "/src/Images/PacMan/pacman_left.gif");
-        windows.addImage(this.scene, "pacManRight", curDir + "/src/Images/PacMan/pacman_right.gif");
-        windows.addImage(this.scene, "pacManUp", curDir + "/src/Images/PacMan/pacman_up.gif");
-        windows.addImage(this.scene, "pacManDown", curDir + "/src/Images/PacMan/pacman_down.gif");
-        windows.addImage(this.scene, "pacManDeath", curDir + "/src/Images/PacMan/pacman_death.gif");
+        windows.addImage(this.scene, imageLabels[0], "./src/sample/Images/PacMan/pacman_left.gif");
+        windows.addImage(this.scene, imageLabels[1], "./src/sample/Images/PacMan/pacman_right.gif");
+        windows.addImage(this.scene, imageLabels[2], "./src/sample/Images/PacMan/pacman_up.gif");
+        windows.addImage(this.scene, imageLabels[3], "./src/sample/Images/PacMan/pacman_down.gif");
+        windows.addImage(this.scene, imageLabels[4], "./src/sample/Images/PacMan/pacman_death.gif");
+    }
+
+    // Display Pac-Man
+    public void displayPacManLeft (int x, int y) throws Exception {
+        windows.setPositionImage(this.scene, imageLabels[0], x, y);
+        windows.displayImage    (this.scene, imageLabels[0]);
     }
 
     public void displayPacManRight (int x, int y) throws Exception {
-        windows.setPositionImage(this.scene,"pacManRight",x,y);
-        windows.displayImage(this.scene,"pacManRight");
-    }
-
-    public void displayPacManLeft (int x, int y) throws Exception {
-        windows.setPositionImage(this.scene, "pacManLeft", x, y);
-        windows.displayImage(this.scene, "pacManLeft");
+        windows.setPositionImage(this.scene, imageLabels[1], x, y);
+        windows.displayImage    (this.scene, imageLabels[1]);
     }
 
     public void displayPacManUp (int x, int y) throws Exception {
-        windows.setPositionImage(this.scene, "pacManUp", x, y);
-        windows.displayImage(this.scene, "pacManUp");
+        windows.setPositionImage(this.scene, imageLabels[2], x, y);
+        windows.displayImage    (this.scene, imageLabels[2]);
     }
 
     public void displayPacManDown (int x, int y) throws Exception {
-        windows.setPositionImage(this.scene, "pacManDown", x, y);
-        windows.displayImage(this.scene, "pacManDown");
+        windows.setPositionImage(this.scene, imageLabels[3], x, y);
+        windows.displayImage    (this.scene, imageLabels[3]);
     }
 
     public void displayPacManDeath (int x, int y) throws Exception {
-        windows.setPositionImage(this.scene, "pacManDeath", x, y);
-        windows.displayImage(this.scene, "pacManDeath");
+        windows.setPositionImage(this.scene, imageLabels[4], x, y);
+        windows.displayImage    (this.scene, imageLabels[4]);
+    }
+
+    // Hide Pac-Man
+    public void hidePacManLeft () throws Exception {
+        windows.hideImage(this.scene, imageLabels[0]);
+    }
+
+    public void hidePacManRight () throws Exception {
+        windows.hideImage(this.scene, imageLabels[1]);
+    }
+
+    public void hidePacManUp() throws Exception {
+        windows.hideImage(this.scene, imageLabels[2]);
+    }
+
+    public void hidePacManDown() throws Exception {
+        windows.hideImage(this.scene, imageLabels[3]);
+    }
+
+    public void hidePacManDeath() throws Exception {
+        windows.hideImage(this.scene, imageLabels[4]);
     }
 }
