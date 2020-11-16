2a4db21373a6cd47f57ab747fca06d532a6e98c6
JulienAN <60385286+JulienAN@users.noreply.github.com>
2020-11-16 20:29:03 +0100

    Add files via upload

---
+++ src/JeuxPacman/DisplayBlinky.java
@@ -0,0 +1,54 @@
+package sample;
+
+import sample.GraphicEngine.GraphicEngine;
+
+import java.util.Arrays;
+
+public class DisplayBlinky {
+    private final String scene;
+    private final GraphicEngine windows;
+    private final String[] imageLabels = {"blinky_left","blinky_right","blinky_up","blinky_down","blinky_edible",
+            "eyes_left","eyes_right","eyes_up","eyes_down"};
+    private String imageCurrent= "";
+
+    public DisplayBlinky(GraphicEngine windows, String nameScene) throws Exception {
+        this.windows = windows;
+        this.scene   = nameScene;
+
+        windows.addImage(this.scene, imageLabels[0], "./src/sample/Images/Ghosts/red/ghost_red_left.gif");
+        windows.addImage(this.scene, imageLabels[1], "./src/sample/Images/Ghosts/red/ghost_red_right.gif");
+        windows.addImage(this.scene, imageLabels[2], "./src/sample/Images/Ghosts/red/ghost_red_up.gif");
+        windows.addImage(this.scene, imageLabels[3], "./src/sample/Images/Ghosts/red/ghost_red_down.gif");
+
+        windows.addImage(this.scene, imageLabels[4], "./src/sample/Images/Ghosts/edible/ghost_edible.gif");
+
+        windows.addImage(this.scene, imageLabels[5], "./src/sample/Images/Ghosts/eyes/eyes_left.png");
+        windows.addImage(this.scene, imageLabels[6], "./src/sample/Images/Ghosts/eyes/eyes_right.png");
+        windows.addImage(this.scene, imageLabels[7], "./src/sample/Images/Ghosts/eyes/eyes_up.png");
+        windows.addImage(this.scene, imageLabels[8], "./src/sample/Images/Ghosts/eyes/eyes_down.png");
+    }
+
+    // Display Blinky
+    public void displayBlinky(String label, int x, int y) throws Exception {
+        if (Arrays.asList(imageLabels).contains(label) || label == "") {
+
+            if (Arrays.asList(imageLabels).contains(imageCurrent)) {
+                windows.hideImage(this.scene, imageCurrent);
+            }
+
+            windows.setPositionImage(this.scene, label, x, y);
+            windows.displayImage(this.scene, label);
+
+            imageCurrent = label;
+        }
+        else {
+            System.out.println("Mauvais label dans DisplayOrange");
+        }
+    }
+
+    // Hide Blinky
+    public void hideBlinky () throws Exception {
+        if (Arrays.asList(imageLabels).contains(imageCurrent))
+            windows.hideImage(this.scene, imageCurrent);
+    }
+}
---
+++ src/JeuxPacman/DisplayPacman.java
@@ -0,0 +1,51 @@
+package sample;
+
+import sample.GraphicEngine.GraphicEngine;
+
+import java.util.Arrays;
+
+public class DisplayPacman {
+
+    private final String scene;
+    private final GraphicEngine windows;
+    private final String[] imageLabels = {"Pacman_left","Pacman_right","Pacman_up","Pacman_down","Pacman_death"};
+
+    private String imageCurrent= "";
+
+
+    public DisplayPacman(GraphicEngine windows, String nameScene) throws Exception {
+        this.windows = windows;
+        this.scene   = nameScene;
+
+        windows.addImage(this.scene, imageLabels[0], "./src/sample/Images/PacMan/pacman_left.gif");
+        windows.addImage(this.scene, imageLabels[1], "./src/sample/Images/PacMan/pacman_right.gif");
+        windows.addImage(this.scene, imageLabels[2], "./src/sample/Images/PacMan/pacman_up.gif");
+        windows.addImage(this.scene, imageLabels[3], "./src/sample/Images/PacMan/pacman_down.gif");
+        windows.addImage(this.scene, imageLabels[4], "./src/sample/Images/PacMan/pacman_death.gif");
+    }
+
+    // Display Pac-Man
+    public void displayPacMan(String label, int x, int y) throws Exception {
+        if (Arrays.asList(imageLabels).contains(label) || label == "") {
+
+            if (Arrays.asList(imageLabels).contains(imageCurrent)) {
+                windows.hideImage(this.scene, imageCurrent);
+            }
+
+            windows.setPositionImage(this.scene, label, x, y);
+            windows.displayImage(this.scene, label);
+
+            imageCurrent = label;
+        }
+        else {
+            System.out.println("Mauvais label dans DisplayPacman");
+        }
+    }
+
+    // Hide Pac-Man
+    public void hidePacMan () throws Exception {
+        if (Arrays.asList(imageLabels).contains(imageCurrent))
+            windows.hideImage(this.scene, imageCurrent);
+    }
+
+}
